264aa65f4fe4ca7a2e50b29893f419526c2352b7
Fahry-a <farhannzarm@gmail.com>
2026-06-18 18:55:41 +0700

    ini

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Oryn-Tunnelv2"
---
+++ src/main/java/net/oryn/mc/orynTunnelv2/OrynTunnelv2.java
@@ -0,0 +1,16 @@
+package net.oryn.mc.orynTunnelv2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OrynTunnelv2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
