ed0e4a11078672bfc15025aeb600e1c1908017da
Fahry-a <farhannzarm@gmail.com>
2026-06-20 10:15:54 +0700

    Add publish to personal maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("maven-publish")
     id("xyz.jpenilla.run-paper") version "3.0.2"
     id("com.gradleup.shadow") version "9.4.2"
 }
@@ -18,6 +19,51 @@ dependencies {
 
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "oryntunnelv2"
+            version = project.version.toString()
+
+            artifact(tasks.shadowJar)
+
+            pom {
+                name.set("Oryn-Tunnelv2")
+                description.set("A Cloudflare Tunnel Plugin For Tunneling Web To Cloudflare")
+                url.set("https://github.com/Fahry-a/Oryn-Tunnelv2")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Fahry-a")
+                        name.set("Fahry-a")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Fahry-a/Oryn-Tunnelv2.git")
+                    developerConnection.set("scm:git:ssh://github.com/Fahry-a/Oryn-Tunnelv2.git")
+                    url.set("https://github.com/Fahry-a/Oryn-Tunnelv2")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "LocalRepo"
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
 }
 
 tasks {
